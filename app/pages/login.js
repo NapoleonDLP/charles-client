eab9276e07036f6e1f7ca5c3e43286226b6dbf43
NapoleonDLP <napoleon.dlp@gmail.com>
2021-05-04 02:12:45 -0700

    Implemented the fetch request and handlechange

--- app/pages/login.js
+++ app/pages/login.js
@@ -3,14 +3,29 @@ import React, {useEffect, useState} from 'react';
 export default function Login() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [credentials, setCredentials] = useState({
-    username: '',
+    email: '',
     password: ''
   });
+  const [user, setUser] = useState(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-
+    fetch('http://localhost:3001/authentication', {
+      method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials)
+    })
+    .then(data => data.json())
+    //TODO: Adjust format to conform to the state. ex string with response vs user model, Idea. Return an json and have
+    //keys equal isLoggedIn and user?
+    .then(user => {
+      setUser(user);
+      setIsLoggedIn(true);
+    })
+    .catch((error) => console.error('Error: '.error))
   }
 
   const handleChange = (event) => {
@@ -24,12 +39,10 @@ export default function Login() {
       <h1>Login</h1>
     <form className='login-form' onSubmit={handleSubmit}>
       <label>
-        <input onChange={handleChange} placeholder='Username' value={credentials.username} type='text' name='username'/>
-      </label>
-      <label>
+        <input onChange={handleChange} placeholder='email' value={credentials.email} type='text' name='email'/>
         <input onChange={handleChange} placeholder='Password' value={credentials.password} type='text' name='password' />
       </label>
-      <input type='button' value='Submit' name='submit' />
+      <input type='submit' name='submit' />
     </form>
     </div>
   )
