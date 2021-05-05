d06bc2701e63520b087b0bf7043550b0f8bccc5e
NapoleonDLP <napoleon.dlp@gmail.com>
2021-05-04 22:42:50 -0700

    Added sign-in link registered users

--- app/pages/signup.js
+++ app/pages/signup.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 export default function Signup() {
   const [user, setUser] = useState({
@@ -14,7 +15,7 @@ export default function Signup() {
   }
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
+    // event.preventDefault();
     fetch('http://localhost:3001/users', {
       method: 'POST',
       headers: {
@@ -30,21 +31,28 @@ export default function Signup() {
   }
 
   return (
-    <form className='signup-form' onSubmit={handleSubmit}>
-      <label>
-        <h1> Create Your Account </h1>
-      </label>
-        First Name
-        <input onChange={handleChange} type='text' name='first_name' className='form-field' placeholder='First Name' values={user.first_name} />
-        Last Name
-        <input onChange={handleChange} type='text' name='last_name' className='form-field' placeholder='Last Name' values={user.last_name} />
-        Email
-        <input onChange={handleChange} type='text' name='email' className='form-field' placeholder='Email' values={user.email} />
-        Slug
-        <input onChange={handleChange} type='text' name='slug' className='form-field' placeholder='Slug' values={user.slug} />
-        Password
-        <input onChange={handleChange} type='text' name='password' className='form-field' placeholder='Password' values={user.password} />
-        {(user.first_name && user.last_name && user.email && user.password) ? <input type='submit' value='Submit'/> : null}
-    </form>
+    <>
+      <form className='signup-form' onSubmit={handleSubmit}>
+        <label>
+          <h1> Create Your Account </h1>
+        </label>
+          First Name
+          <input onChange={handleChange} type='text' name='first_name' className='form-field' placeholder='First Name' values={user.first_name} />
+          Last Name
+          <input onChange={handleChange} type='text' name='last_name' className='form-field' placeholder='Last Name' values={user.last_name} />
+          Email
+          <input onChange={handleChange} type='text' name='email' className='form-field' placeholder='Email' values={user.email} />
+          Slug
+          <input onChange={handleChange} type='text' name='slug' className='form-field' placeholder='Slug' values={user.slug} />
+          Password
+          <input onChange={handleChange} type='text' name='password' className='form-field' placeholder='Password' values={user.password} />
+          {(user.first_name && user.last_name && user.email && user.password) ? <input type='submit' value='Submit'/> : null}
+      </form>
+      <small>Already have an account?
+        <Link href='/login'>
+          <a> Sign-in</a>
+        </Link>
+      </small>
+    </>
   )
 };
